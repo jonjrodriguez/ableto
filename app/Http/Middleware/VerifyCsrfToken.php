72ec10a6de1bf282f05e5b54d677fd4f8ed76471
Jonathan Rodriguez <jonathan.rodriguez@nyu.edu>
2017-10-08 17:50:06 -0400

    Initial commit

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AbleTo\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array
+     */
+    protected $except = [
+        //
+    ];
+}
