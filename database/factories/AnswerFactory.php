c3b2dda5989791ae153ff98503534eea6c557ed7
Jonathan Rodriguez <jonathan.rodriguez@nyu.edu>
2017-10-09 03:23:40 -0400

    Add seeder for answers

--- database/factories/AnswerFactory.php
+++ database/factories/AnswerFactory.php
@@ -6,14 +6,15 @@ use AbleTo\User;
 use Faker\Generator as Faker;
 
 $factory->define(AbleTo\Answer::class, function (Faker $faker) {
-    $question = factory(Question::class)->create();
     return [
         'user_id' => function() {
             return factory(User::class)->create()->id;
         },
-        'question_id' => $question->id,
-        'option_id' => function() use ($question) {
-            return factory(Option::class)->create(['question_id' => $question->id])->id;
+        'question_id' => function() {
+            return factory(Question::class)->create()->id;
+        },
+        'option_id' => function() {
+            return factory(Option::class)->create()->id;
         }
     ];
 });
---
+++ database/seeds/AnswersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use AbleTo\Answer;
+use AbleTo\Question;
+use AbleTo\User;
+use Illuminate\Database\Seeder;
+
+class AnswersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $questions = Question::with('options')->get();
+        $user = User::find(1);
+
+        for ($i = 1; $i < 10; $i++) {
+            $date = \Carbon\Carbon::now()->subDays($i);
+
+            $questions->each(function ($question) use ($user, $date, $i) {
+                factory(Answer::class)->create([
+                   'user_id' => $user->id,
+                   'question_id' => $question->id,
+                   'option_id' => $question->options[$i % 3]->id,
+                   'created_at' => $date
+                ]);
+            });
+        }
+    }
+}
